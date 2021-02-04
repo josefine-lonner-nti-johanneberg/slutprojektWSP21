fa149e6c90e1b5bb4a3d7c322b5847e68db577b7
josefine-lonner-nti-johanneberg <60431270+josefine-lonner-nti-johanneberg@users.noreply.github.com>
2021-02-04 16:06:57 +0100

    lektion 21-02-04
    kopierat över från tidigare arbete.

---
+++ trademarket/views/app.rb
@@ -0,0 +1,95 @@
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+
+enable :sessions
+
+get('/') do
+  slim(:start)
+end
+
+get('/register') do
+  slim(:register)
+end
+
+get('/showlogin') do
+  slim(:login)
+end
+
+post('/user/new') do
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+
+  if (password == password_confirm)
+   
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new("db/todo2021.db")
+    db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+    redirect('/')
+  else
+    
+    "Lösenorden matchade inte!"
+  end
+end
+
+post('/login') do
+  username = params[:username]
+  password = params[:password]
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+  pwdigest = result["pwdigest"]
+  id = result["id"]
+
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id
+    redirect('/todos')
+  else
+    "Fel lösenord!"
+  end
+end
+
+get('/todos') do
+  id = session[:id].to_i
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
+  slim(:"todos/index",locals:{todos:result})
+end
+
+post('/todos/new') do
+  content = params[:content]
+  user_id = params[:user_id]
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.execute("INSERT INTO todos (content,user_id) VALUES (?,?)",content,user_id)
+  redirect('/todos')
+end
+
+post('/todos/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.execute("DELETE FROM todos WHERE id = ?",id)
+  redirect('/todos')
+end
+
+post('/todos/:id/update') do
+  id = params[:id].to_i
+  content = params[:content]
+
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.execute("UPDATE todos SET content = ? where id = ?",content,id)
+  redirect('/todos')
+end
+
+get('/todos/:id/edit') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM todos WHERE id = ?",id).first
+  p "result är #{result}"
+  slim(:"/todos/edit", locals:{result:result})
+end
+
