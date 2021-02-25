f01d505424b32600387711e41e6ab3b7a4479e19
josefine-lonner-nti-johanneberg <60431270+josefine-lonner-nti-johanneberg@users.noreply.github.com>
2021-02-25 15:10:59 +0100

    lektion 25-02-21
    Ändringar i bla css och formulär

--- trademarket/views/app.rb
+++ trademarket/views/app.rb
@@ -13,13 +13,13 @@ end
 post('/login') do
   mail = params[:mail]
   password = params[:password]
-  db = SQLite3::Database.new("db/todo2021.db")
+  db = SQLite3::Database.new("db/Trademarket.db")
   db.results_as_hash = true
-  result = db.execute("SELECT * FROM users WHERE username = ?",username).first
-  pwdigest = result["pwdigest"]
+  result = db.execute("SELECT * FROM users WHERE name = ?",name).first
+  password = result["password"]
   id = result["id"]
 
-  if BCrypt::Password.new(pwdigest) == password
+  if BCrypt::Password.new(password) == password
     session[:id] = id
     redirect('/annonser')
   else
@@ -32,60 +32,60 @@ get('/register') do
 end
 
 post('/user/new') do
-  username = params[:username]
+  name = params[:name]
   password = params[:password]
   password_confirm = params[:password_confirm]
 
   if (password == password_confirm)
    
     password_digest = BCrypt::Password.create(password)
-    db = SQLite3::Database.new("db/todo2021.db")
-    db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
-    redirect('/')
+    db = SQLite3::Database.new("db/Trademarket.db")
+    db.execute("INSERT INTO users (name,mail,password) VALUES (?,?,?)",name,mail,password_digest)
+    redirect('/annonser')
   else
     
     "Lösenorden matchade inte!"
   end
 end
 
-get('/todos') do
+get('/annonser') do
   id = session[:id].to_i
-  db = SQLite3::Database.new("db/todo2021.db")
+  db = SQLite3::Database.new("db/Trademarket.db")
   db.results_as_hash = true
-  result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
-  slim(:"todos/index",locals:{todos:result})
+  result = db.execute("SELECT * FROM annonser WHERE user_id = ?",user_id)
+  slim(:"annonser/index",locals:{annonser:result})
 end
 
-post('/todos/new') do
-  content = params[:content]
+post('/annonser/new') do
+  item = params[:item]
   user_id = params[:user_id]
-  db = SQLite3::Database.new("db/todo2021.db")
-  db.execute("INSERT INTO todos (content,user_id) VALUES (?,?)",content,user_id)
+  db = SQLite3::Database.new("db/Trademarket.db")
+  db.execute("INSERT INTO annonser (item,user_id) VALUES (?,?)",item,user_id)
+  redirect('/annonser')
+end
+
+post('/annonser/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/Trademarket.db")
+  db.execute("DELETE FROM annonser WHERE annons_id = ?",annons_id)
   redirect('/todos')
 end
 
-post('/todos/:id/delete') do
-  id = params[:id].to_i
-  db = SQLite3::Database.new("db/todo2021.db")
-  db.execute("DELETE FROM todos WHERE id = ?",id)
-  redirect('/todos')
+post('/annonser/:id/update') do
+  annons_id = params[:annons_id].to_i
+  item = params[:item]
+
+  db = SQLite3::Database.new("db/Trademarket.db")
+  db.execute("UPDATE annonser SET item = ? where annons_id = ?",item,annons_id)
+  redirect('/annonser')
 end
 
-post('/todos/:id/update') do
-  id = params[:id].to_i
-  content = params[:content]
-
-  db = SQLite3::Database.new("db/todo2021.db")
-  db.execute("UPDATE todos SET content = ? where id = ?",content,id)
-  redirect('/todos')
-end
-
-get('/todos/:id/edit') do
-  id = params[:id].to_i
-  db = SQLite3::Database.new("db/todo2021.db")
+get('/annonser/:id/edit') do
+  annons_id = params[:annons_id].to_i
+  db = SQLite3::Database.new("db/Trademarket.db")
   db.results_as_hash = true
-  result = db.execute("SELECT * FROM todos WHERE id = ?",id).first
+  result = db.execute("SELECT * FROM annonser WHERE id = ?",annons_id).first
   p "result är #{result}"
-  slim(:"/todos/edit", locals:{result:result})
+  slim(:"/annonser/edit", locals:{result:result})
 end
 
